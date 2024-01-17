26382b5d5eaa589fe19bc86a8012dde2ed2f0a8a
acascoc098 <acascoc098@g.educaand.es>
2024-01-17 14:41:01 +0100

    Intento de meter un máximo y un mínimo por un input.

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -6,13 +6,26 @@ const Counter = () => {
 
     //Todos los estados deben estar dento de un componente
     const [counter, setCounter] = useState(0);
+    function TextInput() {
+        const [text, setText] = useState('');
+        function handleChange(event) {
+    setText(event.target.value);}
+    }
 
     console.log("Renderizando counter: "+ counter);
     return(
         <div>
+            <p>Indica el máximo:</p>
+            <input type="text" value={text} onChange={handleChange}></input>
+            <p>Indica el mínimo:</p>
+            <input type="text" id="min"></input>
             <p> El contador es: <a>{counter}</a></p>
-            <button onClick={()=>{setCounter(counter+1);console.log(counter);}}>Incrementar</button>
-            <button onClick={()=>{setCounter(counter-1);console.log(counter);}}>Decrementar</button>
+            <button onClick={()=>{if (counter<={text}) {
+                setCounter(counter+1)};
+                console.log(counter);}}>Incrementar</button>
+            <button onClick={()=>{if(counter > 0){
+                setCounter(counter-1)};
+                console.log(counter);}}>Decrementar</button>
             <button onClick={()=>{setCounter(0);console.log(counter);}}>Reset</button>
         </div>
 
