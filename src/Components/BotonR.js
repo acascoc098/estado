e36654f9ed8c0f9eff0bc6c2b0802f0803b8c737
acascoc098 <acascoc098@g.educaand.es>
2024-01-18 13:58:08 +0100

    Cambiar colores con un array y un botón.

--- src/Components/BotonR.js
+++ src/Components/BotonR.js
@@ -5,12 +5,21 @@ const BotonR = () => {
     //Con boleano
     const [color2, setColor2] = useState(true);
 
+    const colores = ["black","white","green","yellow","blue"];
+
+    //Con array
+    const [color3, setColor3] = useState("blue");
+
     return <>
         <button onClick={() => setColor("green")}>Cambiar</button>
         <b style={{color: color}}>Andrea Castilla Cocera</b>
 
         <h1 style={{backgroundColor: color2 ? "blue" : "red", color: "white"}}>Hello Style!</h1>
         <button onClick={() => {setColor2(!color2)}}>Cambiar boolean</button>
+
+        <br></br>
+        <button onClick={() => {setColor3(colores[Math.floor(Math.random() * 5)])}}> Cambiar array</button>
+        <i style={{color: color3, backgroundColor: "ligthgrey"}}>Prueba con el array</i>
     </>
 }
 
