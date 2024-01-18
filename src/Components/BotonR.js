8fc96d8d023ebbc0a48591ef2e2bf0e9de36d216
acascoc098 <acascoc098@g.educaand.es>
2024-01-18 13:47:34 +0100

    Ejemplo de boton para cabiar el color con un boolean.

--- src/Components/BotonR.js
+++ src/Components/BotonR.js
@@ -1,11 +1,16 @@
 import { useState } from "react"
 
 const BotonR = () => {
-    const [color, setColor] = useState("black");
+    const [color, setColor] = useState("blue");
+    //Con boleano
+    const [color2, setColor2] = useState(true);
 
     return <>
         <button onClick={() => setColor("green")}>Cambiar</button>
         <b style={{color: color}}>Andrea Castilla Cocera</b>
+
+        <h1 style={{backgroundColor: color2 ? "blue" : "red", color: "white"}}>Hello Style!</h1>
+        <button onClick={() => {setColor2(!color2)}}>Cambiar boolean</button>
     </>
 }
 
